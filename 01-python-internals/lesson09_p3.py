038e12ff70b565d4e11e051445b6dd23a412784e
Rajan414 <rajandhami18@gmail.com>
2026-08-14 00:51:32 +0545

    complete lesson 09: part 3

--- 01-python-internals/lesson09_p3.py
+++ 01-python-internals/lesson09_p3.py
@@ -19,8 +19,24 @@ def print_student_averages(students):
     for student in students:
         v=calculate_average(student["marks"])
         print (f"{student["name"]} => {v:.2f}")
+    a,b = get_top_student(students)
+    print (f" highest : {b} => {a}")
+        
     
-    
+def get_top_student(students):
+    highest = 0
+    top = ""
+    for student in students:
+        current_avg = calculate_average(student["marks"])
+        if current_avg > highest:
+            highest = current_avg
+            top = student["name"]
+    return highest, top
+          
+        
+          
+
+        
 result = print_student_averages(students = [
     {"name": "Dai", "marks": [85, 90, 78]},
     {"name": "Ram", "marks": [42, 55, 48]},
