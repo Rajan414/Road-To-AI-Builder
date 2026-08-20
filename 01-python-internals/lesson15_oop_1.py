452ba10c0953e5d9d657ab8098ed18a9ca4704c2
Rajan414 <rajandhami18@gmail.com>
2026-08-20 22:35:04 +0545

    complete lesson 15 : part 1 OOP

---
+++ 01-python-internals/lesson15_oop_1.py
@@ -0,0 +1,12 @@
+# try
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+
+    def calculate_average(self):
+        return sum(self.marks) / len(self.marks)
+    
+student = Student("Dai", [90, 85, 78])
+
+print(student.calculate_average())
