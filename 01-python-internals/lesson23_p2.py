692aa0a62d85fa8c0464aafb517953ab0751c642
Rajan414 <rajandhami18@gmail.com>
2026-09-01 16:43:17 +0545

    completed lesson 23 part 2

---
+++ 01-python-internals/lesson23_p2.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def insert_first(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+        
+    def display(self):
+        current = self.head
+        while current is not None:
+            print(current.data)
+            current = current.next
+            
+    def search(self, target):
+        current = self.head
+        while current is not None:
+           if current.data ==target :
+               return True
+           else: 
+               current = current.next
+        return False
+    
+def is_empty(self):
+    if self.head is None:
+        return True
+    return False
+
+my_list = LinkedList()
+my_list.is_empty()
+my_list.insert_first(10)
+my_list.insert_first(20)
+my_list.insert_first(30)
