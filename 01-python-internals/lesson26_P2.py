823a9f6dd355bca64d921f63e4e8e72b2a662d2e
Rajan414 <rajandhami18@gmail.com>
2026-09-04 17:45:03 +0545

    lesson 26

---
+++ 01-python-internals/lesson26_P2.py
@@ -0,0 +1,10 @@
+def has_cycle(self):
+        slow = self.head
+        fast = self.head
+        while fast is not None and fast.next is not None:
+           slow = slow.next
+           fast = fast.next.next
+           if slow == fast:
+               return True
+        return False 
+    
