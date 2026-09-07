4b5d96603585a20df9dc7353ec1def723949823f
Rajan414 <rajandhami18@gmail.com>
2026-09-07 19:34:36 +0545

    lesson 28

---
+++ 01-python-internals/lesson28_p2.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+def remove_nth_from_end(self, n):
+    dummy = Node(0)
+    dummy.next = self.head
+    first = dummy 
+    second = dummy
+    for _ in range(n-1):
+        first = first.next
+    while first.next is not None:
+        first = first.next
+        second = second.next
+    second.next = second.next.next
+    self.head = dummy.next
+    
