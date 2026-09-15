5a9cd16dcb3416d9a37cf706c94529ec73cbb31c
Rajan414 <rajandhami18@gmail.com>
2026-09-15 17:46:45 +0545

    lesson 32

---
+++ 01-python-internals/lesson32_p2.py
@@ -0,0 +1,25 @@
+class PriorityQueue:
+    def __init__(self):
+        self.heap = MinHeap()
+
+    def enqueue(self, priority, task):
+        self.heap.push((priority, task))
+
+    def dequeue(self):
+        return self.heap.pop()
+
+    def peek(self):
+        if not self.heap.heap:
+            return None
+
+        return self.heap.heap[0]
+pq = PriorityQueue()
+
+pq.enqueue(5, "Exercise")
+pq.enqueue(1, "Study")
+pq.enqueue(3, "Lunch")
+pq.enqueue(2, "Assignment")
+print(pq.dequeue())
+print(pq.dequeue())
+print(pq.dequeue())
+print(pq.dequeue())
